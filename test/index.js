813193311fea73d3233d69085e597f4323c8bf15
Sam Goto <goto@goto-macbookair2.roam.corp.google.com>
2018-05-04 12:26:23 -0700

    adds a simple RSS parser with a simple AR extension and connects it with the selector matching code

--- test/index.js
+++ test/index.js
@@ -1,8 +1,11 @@
 const Assert = require("assert");
 const HtmlDom = require('htmldom');
+const fs = require('fs');
+const Parser = require('rss-parser');
 
 describe("Doc tests", function(done) {
   it("Simple text", function() {
+
     let dom = new HtmlDom(`
   <person gender="male">
     <face>
@@ -12,6 +15,27 @@ describe("Doc tests", function(done) {
   </person>
   `);
 
-    console.log(dom.$("person[gender='male'] eye[right]"));
+
+    let parser = new Parser({
+      customFields: {
+       "item": ["ar:artifact"]
+      }
+     });
+
+    var content = fs.readFileSync('./test/example.xml', 'utf8');
+    parser.parseString(content).then(rss => {
+      for (let item of rss.items) {
+       let artifact = item["ar:artifact"];
+       let selector = artifact["ar:anchor"][0]["$"].selector;
+       let asset = artifact["ar:asset"][0]["$"].src;
+
+       let matches = dom.$(selector);
+       console.log(`Add to ${selector}: ${asset}`);
+       console.log(matches);
+      }
+    });
+
+    return;
+
   });
 });
